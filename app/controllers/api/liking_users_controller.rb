29decc28f0a834d77edac16d7bffef47252b9982
taichihub <taichikuniv21@gmail.com>
2024-11-28 18:04:21 +0900

    APIレスポンスにメールアドレスが含まれないようにした

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -3,8 +3,10 @@
 class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
-    @users = blog.liking_users.order(:id)
+    @users = blog.liking_users.order(:id).as_json(except: :email)
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
+    response_data = { users: @users, destroy_path: @destroy_path }
+    render json: response_data
   end
 end
