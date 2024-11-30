da0a82bf3547e78931cb7b319be5ad58d1756ee3
taichihub <taichikuniv21@gmail.com>
2024-11-30 18:01:48 +0900

    jsonの情報を取得するロジックを修正

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -3,10 +3,11 @@
 class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
-    @users = blog.liking_users.order(:id).as_json(except: :email)
+    @users = blog.liking_users.order(:id)
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
-    response_data = { users: @users, destroy_path: @destroy_path }
+    users_json = @users.as_json(only: %i[id created_at updated_at nickname premium])
+    response_data = { users: users_json, destroy_path: @destroy_path }
     render json: response_data
   end
 end
