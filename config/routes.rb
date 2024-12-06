714ad704532beeb39df5b25e13cc137963dbc8d3
taichihub <taichikuniv21@gmail.com>
2024-12-06 18:03:47 +0900

    デフォルトのフォーマットをjsonにした

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :blogs, only: [] do
       resources :likings, only: %i(create destroy)
-      resources :liking_users, only: :index
+      resources :liking_users, only: :index, defaults: { format: :json }
     end
   end
 end
